f12e6b8c2e71ad03cd1b42ffdaf9c5501dc3f236
feng.du <feng.du@uniigym.com>
2024-02-29 15:36:08 +0800

    设计模式案例

--- src/main/java/learn/design/patterns/demo/facade/Test.java
+++ src/main/java/learn/design/patterns/demo/facade/Test.java
@@ -2,25 +2,22 @@ package learn.design.patterns.demo.facade;
 
 /**
  * @Author df
- * @Description: TODO
+ * @Description:
  * @Date 2024/2/17 16:20
  */
 public class Test {
 
+    //如果不加门面模式
     public void before(){
         new DateValidateRule();
         new FunctionValidateRule();
     }
 
-
+    // 如果加门面模式
     public void after(){
         ValidateRule validateRule=   new ValidateRule();
         validateRule.dateGreaterValidate();
         validateRule.isContentNull();
-
-    }
-
-    public static void main(String[] args) {
 
     }
 }
